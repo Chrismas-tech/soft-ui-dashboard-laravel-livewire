1351df7a91b7503421001c471c05925e5a039e17
Chrismas-tech <71583547+Chrismas-tech@users.noreply.github.com>
2023-01-25 14:02:21 +0100

    vite working

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- Metas -->
-    @if(env('IS_DEMO'))
+    @if (env('IS_DEMO'))
         <x-demo-metas></x-demo-metas>
     @endif
     <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
@@ -26,7 +26,6 @@
     <!-- Alpine -->
     <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
     @livewireStyles
-
 </head>
 
 <body class="g-sidenav-show bg-gray-100">
@@ -45,7 +44,6 @@
             }
             Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
         }
-
     </script>
     <!-- Github buttons -->
     <script async defer src="https://buttons.github.io/buttons.js"></script>
